71a30072d5bef1913a5b14a59649d7f66452c759
YanaVG <yanavg18@gmail.com>
2018-08-24 08:34:25 +0300

    corrected reducer

--- HomeTask#3/hw3/src/redux/reducers/index.js
+++ HomeTask#3/hw3/src/redux/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import filterReducer from './filter';
 import itemsReducer from './movies-list';
 
 const rootReduser = combineReducers({
-  filter: filterReducer,
   movies: itemsReducer,
 });
 
