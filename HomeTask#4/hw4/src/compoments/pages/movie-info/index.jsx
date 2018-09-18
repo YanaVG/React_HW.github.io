5dc2c278615761b0371d36b0c6a085397cc79856
YanaVG <yanavg18@gmail.com>
2018-09-18 10:10:25 +0300

    added movies page

---
+++ HomeTask#4/hw4/src/compoments/pages/movie-info/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import searchById from '../../../secvices/search-by-id';
+
+export default class MovieInfo extends Component {
+  
+  render(){
+      return(
+          <div></div>
+      )
+  }
+} 
