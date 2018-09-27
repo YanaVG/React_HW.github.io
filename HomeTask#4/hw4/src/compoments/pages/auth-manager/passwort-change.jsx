f488b8ac30606ec1bd6b2f3c767f22d007d428e4
YanaVG <yanavg18@gmail.com>
2018-09-27 20:41:17 +0300

    Add auth

---
+++ HomeTask#4/hw4/src/compoments/pages/auth-manager/passwort-change.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import input from '@material-ui/core/Input';
+import Button from '../../shared-ui/'
---
+++ HomeTask#4/hw4/src/compoments/pages/auth-manager/sign-in-page.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import firebase from 'firebase';
+import firebaseui from 'firebaseui';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import * as routes from '../../../constants/routes';
+import styles from './style.css';
+
+class SignInPage extends Component {
+  uiConfig = {
+    signInFlow: 'popup',
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: () => {
+        const { history } = this.props;
+        history.push(routes.HOME);
+      },
+    },
+  }
+
+  render() {
+      return (
+        <div className={styles.signIn_container}>
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        </div>
+      )
+  }
+};
+
+SignInPage.propTypes = {
+  history: PropTypes.objectOf(Object).isRequired,
+};
+
+export default withRouter(SignInPage);
